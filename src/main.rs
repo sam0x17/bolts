56107cbda7410a199853182030c779648e1cc9ca
Sam Johnson <sam@durosoft.com>
2021-06-07 02:48:54 -0400

    route method complete

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,4 @@ use router::*;
 
 pub fn main() {
     println!("hello world");
-    let mut router = Router::new();
-    router
-        .route(Verb::Post, "/this/is/a/:int/;float/#string")
-        .unwrap();
-    println!("first one ok");
-    router.route(Verb::Patch, "/this/is/invalid:ok/").unwrap();
-    println!("second one ok");
 }
--- src/router.rs
+++ src/router.rs
@@ -1,8 +1,10 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::*;
+use std::hash::*;
+use std::collections::hash_map::DefaultHasher;
 
-#[derive(Debug, PartialEq, Clone, Hash)]
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
 pub enum Verb {
     Get,
     Post,
@@ -12,23 +14,34 @@ pub enum Verb {
     Head,
 }
 
-#[derive(Debug, PartialEq, Clone, Hash)]
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
 enum RouteVar {
     Int(String),
     Float(String),
     String(String),
 }
 
-#[derive(Debug, PartialEq, Clone, Hash)]
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
+enum RoutePart {
+    Path(String),
+    Int,
+    Float,
+    String,
+}
+
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
+struct RouteKey {
+    parts: Vec<RoutePart>,
+}
+
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
 struct Route {
-    path: String,
     vars: Vec<RouteVar>,
-    verb: Verb,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Router {
-    routes: HashMap<String, Route>,
+    routes: HashMap<RouteKey, Route>,
 }
 
 impl Router {
@@ -44,6 +57,37 @@ impl Router {
         }
         if !REG.is_match(path) {
             return Err("invalid route format!");
+        }
+        let mut route_key = RouteKey {
+            parts: Vec::new()
+        };
+        let mut route = Route {
+            vars: Vec::new()
+        };
+        for token in path.split('/') {
+            if token.len() == 0 { continue; }
+            match token.chars().nth(0).unwrap() {
+                ':' => { // integer var
+                    route_key.parts.push(RoutePart::Int);
+                    route.vars.push(RouteVar::Int((&token[1..]).to_string()));
+                }
+                '#' => { // string var
+                    route_key.parts.push(RoutePart::String);
+                    route.vars.push(RouteVar::String((&token[1..]).to_string()));
+                }
+                ';' => { // float var
+                    route_key.parts.push(RoutePart::Float);
+                    route.vars.push(RouteVar::Float((&token[1..]).to_string()));
+                }
+                _ => {
+                    route_key.parts.push(RoutePart::Path(token.to_string()));
+                }
+            }
+        }
+        let size = self.routes.len();
+        self.routes.insert(route_key, route);
+        if size != self.routes.len() - 1 {
+            return Err("a route identical to this one has already been defined!");
         }
         Ok(())
     }
